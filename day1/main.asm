7178439e8898db46887048a395849cca3baacd19
Eric Zhang <ekzhang1@gmail.com>
2023-12-01 11:26:40 -0500

    Solve day1 in arm64 assembly (just 1.8k!)

--- day1/main.asm
+++ day1/main.asm
@@ -5,57 +5,63 @@ _main:
     stp x29, x30, [sp, -16]!
     mov x29, sp
 
-    adr x0, message
-    bl _printf  // printf(message)
-
-    // Allocate 32 bytes on the stack
+    // Allocate 64 bytes on the stack
     //  [x29, -8]: array address
-    //  [x29, -16]: loop counter
-    //  [x29, -24]: unused
-    //  [sp]: vararg for printf()
-    sub sp, sp, 32
+    //  [x29, -16]: sum of calibration values (part 1)
+    //  [x29, -24]: sum of calibration values (part 2)
+    //  [x29, -32]: unused
+    //  ...
+    //  [sp]: varargs
+    sub sp, sp, 64
 
-    // Loop 100 times using a counter in [x29, -16]
+    // Allocate the array
+    mov x0, 1000
+    bl _malloc
+    str x0, [x29, -8]
+
+    // Initialize locals
     mov x0, 0
     str x0, [x29, -16]
+    str x0, [x29, -24]
 
-loop_start:
-    ldr x0, [x29, -16]
-    cmp x0, 100
-    b.ge loop_end
-    str x0, [sp]  // vararg for printf()
-    add x0, x0, 1
-    str x0, [x29, -16]
+loop1_start:  // For each line in input
+    adrp x0, scanf1@PAGE
+    add x0, x0, scanf1@PAGEOFF
 
-    // Call malloc on 16 bytes
-    mov x0, 16
-    bl _malloc
-    str x0, [x29, -8]  // [x29, -8] is the address of the array
+    ldr x1, [x29, -8]  // array address...
+    str x1, [sp]       // placed in vararg for scanf()
 
-    mov w1, 'H'
-    strb w1, [x0]
-    mov w1, 'i'
-    strb w1, [x0, 1]
-    mov w1, ' '
-    strb w1, [x0, 2]
-    mov w1, '%'
-    strb w1, [x0, 3]
-    mov w1, 'd'
-    strb w1, [x0, 4]
-    mov w1, '\n'
-    strb w1, [x0, 5]
-    mov w1, 0
-    strb w1, [x0, 6]
+    bl _scanf  // scanf("%999s\n", word);
+    cmp x0, 1
+    b.ne loop1_end
 
-    bl _printf  // printf(array)
+    // Part 1
+    ldr x0, [x29, -8]  // array address
+    // bl _printf  // printf(word);
+    mov x1, 0
+    bl _get_calibration_value
+    ldr x1, [x29, -16]
+    add x1, x1, x0
+    str x1, [x29, -16]
 
-    // Call free on the array
-    ldr x0, [x29, -8]
-    bl _free
+    // Part 2
+    ldr x0, [x29, -8]  // array address
+    mov x1, 1
+    bl _get_calibration_value
+    ldr x1, [x29, -24]
+    add x1, x1, x0
+    str x1, [x29, -24]
 
-    b loop_start
+    b loop1_start
 
-loop_end:
+loop1_end:
+    adrp x0, printf2@PAGE
+    add x0, x0, printf2@PAGEOFF
+    ldr x1, [x29, -16]
+    ldr x2, [x29, -24]
+    stp x1, x2, [sp]
+    bl _printf  // printf("%d\n%d\n", sum1, sum2);
+
     // Set the return value to 0
     mov x0, 0
 
@@ -64,5 +70,128 @@ loop_end:
     ldp x29, x30, [sp], 16
     ret
 
-message:
-    .asciz "Hello, World!\n"
+
+.global _get_calibration_value
+.balign 8
+_get_calibration_value: // int get_calibration_value(const char* word, bool part2)
+    stp x29, x30, [sp, -16]!
+    mov x29, sp
+
+    // Allocate 64 bytes on the stack
+    //  [x29, -8]: current *word pointer
+    //  [x29, -16]: bool part2
+    //  [x29, -24]: first digit in word (or -1)
+    //  [x29, -32]: last digit in word (or -1)
+    //  [x29, -40]: inner loop pointer into *numerals array
+    //  [x29, -48]: numeral temp storage
+    //  [x29, -56]: unused
+    sub sp, sp, 64
+
+    // Initialize locals
+    stp x1, x0, [x29, -16]  // Save arguments
+    mov x0, -1
+    stp x0, x0, [x29, -32]  // w7 = -1, w8 = -1
+
+loop2_start:  // For each character in word
+    ldr x0, [x29, -8]
+    mov x1, 0
+    ldrb w1, [x0], 1  // w1 = *word++
+    str x0, [x29, -8]
+
+    cmp w1, 0
+    b.eq loop2_end
+
+    cmp w1, '0'
+    b.lt loop2_alpha
+    cmp w1, '9'
+    b.gt loop2_alpha
+
+    sub w1, w1, '0'
+    b loop2_found_number
+
+loop2_alpha:  // Check if it is a numeral using string comparison
+    ldr x0, [x29, -16]
+    cmp w0, 0
+    b.eq loop2_start  // part 1, so we skip this step
+
+    adrp x0, numerals@PAGE
+    add x0, x0, numerals@PAGEOFF
+    str x0, [x29, -40]  // Store pointer to numerals array
+
+loop3_start:  // For each numeral in numerals array
+    ldr x0, [x29, -40]
+    ldr x1, [x0], 8  // x1 = *numerals++
+    str x0, [x29, -40]
+
+    cmp x1, 0
+    b.eq loop2_start  // Failed to find a matching numeral, continue
+
+    str x1, [x29, -48]  // Store numeral in temp storage
+    mov x0, x1
+    bl _strlen  // x2 = strlen(numeral)
+    mov x2, x0
+
+    ldr x0, [x29, -8]
+    sub x0, x0, 1  // cancel out the previous increment (oops)
+    ldr x1, [x29, -48]
+    bl _strncmp  // strncmp(word, numeral, strlen(numeral));
+    cmp x0, 0
+    b.ne loop3_start  // Go to the next numeral
+
+    // Found a matching numeral
+    adrp x0, numerals@PAGE
+    add x0, x0, numerals@PAGEOFF
+    ldr x1, [x29, -40]
+    sub x1, x1, x0  // x1 = index of numeral in numerals array
+    mov x0, 8
+    udiv x1, x1, x0
+    b loop2_found_number
+
+loop2_found_number:  // The numeric digit is now stored in w1
+    ldr x0, [x29, -24]
+    cmp w0, -1
+    b.ne loop2_if_skip
+    str x1, [x29, -24]
+loop2_if_skip:
+    str x1, [x29, -32]
+    b loop2_start
+
+loop2_end:
+    ldp x8, x7, [x29, -32]
+    mov x0, 10
+    mul w0, w0, w7
+    add w0, w0, w8
+
+    mov sp, x29
+    ldp x29, x30, [sp], 16
+    ret
+
+
+.data
+scanf1:
+    .asciz "%999s\n"
+printf2:
+    .asciz "%d\n%d\n"
+
+// Numerals for part 2
+one:
+    .asciz "one"
+two:
+    .asciz "two"
+three:
+    .asciz "three"
+four:
+    .asciz "four"
+five:
+    .asciz "five"
+six:
+    .asciz "six"
+seven:
+    .asciz "seven"
+eight:
+    .asciz "eight"
+nine:
+    .asciz "nine"
+.balign 8
+numerals:
+    .xword one, two, three, four, five, six, seven, eight, nine, 0
